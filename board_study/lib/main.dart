2dcec87ca614ff6aabba0850d34da41edd69faa1
wisehyun0814 <ptuguitar@naver.com>
2021-08-08 09:16:51 +0900

    BottomNavigationBar

--- board_study/lib/main.dart
+++ board_study/lib/main.dart
@@ -29,9 +29,40 @@ class MyHomePage extends StatefulWidget {
 //위 MyHomePageSate 클래스의 상태를 나타내는 state 클래스
 class _MyHomePageState extends State<MyHomePage> {
 //화면에 UI를 그리는 메서드. 그려질 위젯을 반환
+  var _index = 0; //페이지인덱스 0,1,2
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('복잡한 UI')),
+      body: Center(
+        child: Text(
+          '$_index 페이지',
+          style: TextStyle(fontSize: 40),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setSate(() {
+            _index = index;
+          });
+        },
+        currentIndex: _index,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            label: '홈',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: '이용서비스',
+            icon: Icon(Icons.assignment),
+          ),
+          BottomNavigationBarItem(
+            label: '내 정보',
+            icon: Icon(Icons.account_circle),
+          ),
+        ],
+      ),
+    );
   } //Wildget build
 } //class _MyHomePageState extends State<MyHomePage>
