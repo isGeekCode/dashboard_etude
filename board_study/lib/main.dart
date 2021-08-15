07b0900867ca992a8627d22d6852a2a48b629abb
wisehyun0814 <ptuguitar@naver.com>
2021-08-15 09:45:39 +0900

    AppBar 위젯 수정,오른쪽에 메뉴 추가

--- board_study/lib/main.dart
+++ board_study/lib/main.dart
@@ -5,7 +5,7 @@ void main() {
   runApp(MyApp());
 }
 
-//시작클래스 . 머티리얼 디자인 앱 생성
+//시작클래스. 머티리얼 디자인 앱 생성
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -30,24 +30,47 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 //화면에 UI를 그리는 메서드. 그려질 위젯을 반환
   var _index = 0; //페이지인덱스 0,1,2
+  //3개의 페이지를 표현할 수 있도록 _index변수를 준비, 각페이지는 0,1,2의 값을 가진다
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('복잡한 UI')),
+      //build()메서드에 작성된 Container->Scaffold 위젯으로 수정
+
+      appBar: AppBar(
+        backgroundColor: Colors.white, //배경색을 흰색으로
+        title: Text(
+          '복잡한 UI',
+          style: TextStyle(color: Colors.black), //글자를 검정색으로)
+        ),
+        centerTitle: true, //제목을 한가운데로
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.add,
+              color: Colors.black,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+
       body: Center(
         child: Text(
           '$_index 페이지',
           style: TextStyle(fontSize: 40),
         ),
       ),
+
       bottomNavigationBar: BottomNavigationBar(
         onTap: (index) {
-          setSate(() {
-            _index = index;
+          //탭을 클릭하면 onTap 이벤트가 동작하고 선택한 탭의 인덱스전달
+          setState(() {
+            _index = index; //선택된 탭의 인덱스로 _index를 변경
           });
-        },
-        currentIndex: _index,
+        }, //onTap: (index)
+
+        currentIndex: _index, //선택된 인덱스
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             label: '홈',
